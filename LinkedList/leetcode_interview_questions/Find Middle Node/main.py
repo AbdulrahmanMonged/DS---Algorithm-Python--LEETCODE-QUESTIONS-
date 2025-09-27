a7cba99bee103290493eb32568db5082c89382f0
AbdulrahmanMonged <abdoomonged231@gmail.com>
2025-09-27 19:45:02 +0300

    Solved find middle Node problem

---
+++ LinkedList/leetcode_interview_questions/Find Middle Node/main.py
@@ -0,0 +1,55 @@
+# Rule N.1 Only one loop required
+# Rule N.2 Not allowed to either count/get length of LL
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+        
+
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while fast.next or fast != self.tail:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+# my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    3
+    
+"""
