dd14655be00188a2d93e247d3a6f52a8ec18d3bc
AbdulrahmanMonged <abdoomonged231@gmail.com>
2025-11-16 20:42:14 +0200

    added second solution

--- DoublyLinkedList/leetcode_interview_questions/Reverse Between/main.py
+++ DoublyLinkedList/leetcode_interview_questions/Reverse Between/main.py
@@ -56,7 +56,39 @@ class DoublyLinkedList:
             temp2 = temp2.next
             temp2.next, temp1.prev = temp1.prev, temp2.next
             temp1.prev.next = temp1
+            
+    ###################### ANOTHER SOLUTION #################
+    
+    # def reverse_between(self, start_index, end_index):
+    #     if self.length <= 1 or start_index == end_index:
+    #         return
+ 
+    #     dummy = Node(0)
+    #     dummy.next = self.head
+    #     self.head.prev = dummy
+ 
+    #     prev = dummy
+    #     for _ in range(start_index):
+    #         prev = prev.next
+ 
+    #     current = prev.next
+ 
+    #     for _ in range(end_index - start_index):
+    #         node_to_move = current.next
+ 
+    #         current.next = node_to_move.next
+    #         if node_to_move.next:
+    #             node_to_move.next.prev = current
+ 
+    #         node_to_move.next = prev.next
+    #         prev.next.prev = node_to_move
+    #         prev.next = node_to_move
+    #         node_to_move.prev = prev
+ 
+    #     self.head = dummy.next
+    #     self.head.prev = None
 
+    ##########################################  
 
 # Test Cases
 print("\nTest 1: Middle segment reversal")
