80054d202eaec13adfd4c7c520522f489b5c0095
AbdulrahmanMonged <abdoomonged231@gmail.com>
2025-11-18 03:25:40 +0200

    Tree & Stack

--- Stack & Queue/beginning/Queue/main.py
+++ Stack & Queue/beginning/Queue/main.py
@@ -2,3 +2,61 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value=value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+    
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+            
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first:
+            self.last.next = new_node
+            self.last = new_node
+        else:
+            self.first = new_node
+            self.last = new_node
+        self.length += 1
+    
+    def dequeue(self):
+        temp_node = self.first
+        if temp_node:
+            self.first = temp_node.next
+            temp_node.next = None
+            if self.length == 1:
+                self.last = None
+            self.length -= 1
+            
+        return temp_node
+    
+    
+    
+my_queue = Queue(1)
+my_queue.enqueue(2)
+
+# (2) Items - Returns 2 Node
+print(my_queue.dequeue().value)
+# (1) Item -  Returns 1 Node
+print(my_queue.dequeue().value)
+# (0) Items - Returns None
+print(my_queue.dequeue())
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1
+    2
+    None
+
+"""
